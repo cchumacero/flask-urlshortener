baaefc8d182737ca0c9d7f8315ca64c54aa84a35
cchumacero <cnchumacero@gmail.com>
2025-01-23 19:46:00 -0300

    Update routes

---
+++ src/routes/Url.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, jsonify, request, redirect
+from models.url import Url
+from utils.db import db
+from models.urlSchema import UrlSchema
+
+main=Blueprint('url_blueprint', __name__)
+
+@main.route('/urls')
+def get_urls():
+    try:
+        urls = Url.query.all()
+        urls_schema = UrlSchema(many=True)
+        result = urls_schema.dump(urls)
+        return jsonify(result)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+
+@main.route('/', methods=['POST'])
+def get_url():
+    url = request.args.get('url')
+    url_schema = UrlSchema()
+    try:
+        try:
+            short_url = Url.query.filter_by(original_url = url).one()
+        except Exception as NotFound:
+            new_url = Url(url)
+            db.session.add(new_url)
+            db.session.commit()
+            new_url.set_short_url()
+            db.session.commit()
+            short_url = Url.query.filter_by(original_url = url).one()
+        result = url_schema.dump(short_url)        
+        return jsonify(result)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500
+    
+@main.route('/<id>')
+def redirectUrl(id):
+    try:
+        url = Url.query.get(id)
+        if url is not None:
+            url_schema = UrlSchema()
+            result = url_schema.dump(url) 
+            return redirect(result['original_url'])
+        else: return jsonify({'message': "Short Url Does Not Exist"}),404
+    except Exception as ex:
+        return jsonify({'message': str(ex)}),500  

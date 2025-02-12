12d194f3d98554b3be3f054e9733400cb981ae04
cchumacero <cnchumacero@gmail.com>
2025-02-11 21:52:21 -0300

    Add User Schema and validations

---
+++ src/models/userSchema.py
@@ -0,0 +1,25 @@
+from flask_marshmallow import Marshmallow
+from marshmallow import validate, validates, ValidationError
+
+ma = Marshmallow()
+
+class userRegisterSchema(ma.Schema):
+    id = ma.String()
+    username = ma.String(required = True)
+    email = ma.String(required = True, validate=[validate.Email()])
+    password = ma.String(required = True)
+
+class userLoginSchema(ma.Schema):    
+    username = ma.String(required = True)
+    password = ma.String(required = True)
+    
+    @validates('username')
+    def validate_username_or_email(self, value):
+        if '@' in value:
+            try:
+                validate.Email()(value)
+            except ValidationError:
+                raise ValidationError("El email no es valido")
+    
+user_register_schema = userRegisterSchema()
+user_login_schema = userLoginSchema()

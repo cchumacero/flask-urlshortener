563c2eb3390d5bd8a5e803b02a65aa978574f3f3
cchumacero <cnchumacero@gmail.com>
2025-02-11 21:50:26 -0300

    Add User Model

---
+++ src/models/user.py
@@ -0,0 +1,20 @@
+from utils.extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.String(40), primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
+
+    def __init__(self, id, username, email, password):
+        self.id = id,
+        self.username = username,
+        self.email = email
+        self.set_password(password)
+        
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)

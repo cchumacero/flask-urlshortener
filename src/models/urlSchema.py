f3170e9aae2f0b229b206137b02182ce849155fa
cchumacero <cnchumacero@gmail.com>
2025-02-13 22:18:37 -0300

    Change User schema structure

--- src/models/urlSchema.py
+++ src/models/urlSchema.py
@@ -4,9 +4,9 @@ from marshmallow import validate
 ma = Marshmallow()
 
 class UrlSchema(ma.Schema):
-    id = ma.Int()
     original_url = ma.String(required = True, validate=[validate.URL()])
     short_url = ma.String()
+    clicks = ma.Integer()
 
 url_schema = UrlSchema()
 urls_schema = UrlSchema(many=True)

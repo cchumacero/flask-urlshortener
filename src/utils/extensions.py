6be9b4e3604a27a04861b7d75492f1e3f0de51fd
cchumacero <cnchumacero@gmail.com>
2025-02-13 22:27:59 -0300

    Change key_func param of limiter

--- src/utils/extensions.py
+++ src/utils/extensions.py
@@ -3,15 +3,15 @@ from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_jwt_extended import JWTManager
+from flask_limiter.util import get_remote_address
 import os
 
 db = SQLAlchemy()
 ma = Marshmallow()
 migrate = Migrate()
 limiter =  Limiter(
-    key_func=lambda: "global",
+    key_func=get_remote_address,
     storage_uri= os.getenv('UPSTASH_CACHE_URL'),
     default_limits=["1000 per day", "100 per hour"] 
 )
-
 jwt = JWTManager()

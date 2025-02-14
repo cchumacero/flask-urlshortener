fe726c09d539d222ed0a702226f060544ff7c5a3
cchumacero <cnchumacero@gmail.com>
2025-02-14 19:17:23 -0300

    Add production config

--- src/config.py
+++ src/config.py
@@ -2,10 +2,18 @@ from decouple import config
 
 class Config:
     SECRET_KEY=config('SECRET_KEY')
+    DEBUG=False
+    BASE_URL='http://localhost:5000'
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    BASE_URL='http://localhost:5000'
+    
+class ProductionConfig(Config):
+    DEBUG = True
+    BASE_URL='http://tudominio.com'
 
 config = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }

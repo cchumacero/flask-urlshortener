3a0f8c9d948a9f4e9a6ddc71fa8702a9af3362f9
cchumacero <cnchumacero@gmail.com>
2025-02-15 02:17:31 -0300

    Change endpoints to render views

--- src/routes/Url.py
+++ src/routes/Url.py
@@ -1,18 +1,20 @@
-from flask import Blueprint, jsonify, request, redirect, render_template
+from flask import Blueprint, jsonify, request, redirect, render_template, session, flash, url_for
 from models.url import Url
 from utils.extensions import db, limiter
 from models.urlSchema import url_schema, urls_schema
 from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request, jwt_required
 from flask_limiter.util import get_remote_address
 import uuid
+from functools import wraps
 
 
 main=Blueprint('url_blueprint', __name__)
 
 def get_rate_limit_key():
     try:
-        verify_jwt_in_request(optional=True)
-        user_id = get_jwt_identity()
+        # verify_jwt_in_request(optional=True)
+        # user_id = get_jwt_identity()
+        user_id = session.get('user_id')
         if user_id:
             return f"user:{user_id}"
     except:
@@ -21,12 +23,20 @@ def get_rate_limit_key():
     return get_remote_address()
 
 def is_authenticated():
-    verify_jwt_in_request(optional=True)
-    user_id = get_jwt_identity()
+    user_id = session.get('user_id')
     if user_id is None:
         return False
     else:
         return True
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash("Debes iniciar sesión para acceder a esta página", "error")
+            return redirect(url_for('user_route.login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @main.route('/allurls')
 def get_urls():
@@ -38,14 +48,15 @@ def get_urls():
         return jsonify({'message': str(ex)}),500
 
 @main.route('/urls')
-@jwt_required()
+@login_required
 def get_user_urls():
-    user_id = get_jwt_identity()
+    user_id = session.get('user_id')
     
     user_urls = Url.query.filter_by(user_id=user_id).all()
     
     result = urls_schema.dump(user_urls)
     return jsonify(result), 200
+
 
 @main.route('/', methods=['POST'])
 @limiter.limit(
@@ -53,7 +64,6 @@ def get_user_urls():
     key_func=get_rate_limit_key
 )
 def get_url():
-    verify_jwt_in_request(optional=True)
     try:
         url = request.form.get('original_url')
         requested_url = url_schema.load({'original_url': url})
@@ -63,7 +73,7 @@ def get_url():
             
         if short_url is None:
             new_short_url = str(uuid.uuid4())[:8]
-            new_url = Url(url, new_short_url, get_jwt_identity())
+            new_url = Url(url, new_short_url, session.get('user_id'))
             db.session.add(new_url)
             db.session.commit()
             short_url = new_url 
--- src/routes/User.py
+++ src/routes/User.py
@@ -1,35 +1,59 @@
-from flask import Blueprint, jsonify, request, redirect
+from flask import Blueprint, jsonify, request, redirect, render_template, url_for, session, flash
 from models.user import User
 from utils.extensions import db
 from models.userSchema import user_register_schema, user_login_schema
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from marshmallow import ValidationError
 import uuid
+import json
 
 user_route=Blueprint('user_blueprint', __name__)
 
+@user_route.route("/register", methods=['GET'])
+def registerForm():
+    return render_template("register.html")
+
 @user_route.route('/register', methods=['POST'])
 def register():
-    data = user_register_schema.load(request.json)
+    form_username = request.form.get('form_name')
+    form_email = request.form.get('form_email')
+    form_password = request.form.get('form_password')
+    
+    data = user_register_schema.load({
+        "username": form_username,
+        "email": form_email,
+        "password": form_password                           
+    })
     id = str(uuid.uuid4())
     username = data['username']
     email = data['email']
     password = data['password']
-    if User.query.filter_by(username=username).first():
-        return jsonify({"msg": "El usuario ya existe"}), 400
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        flash("El usuario y/o email ya existen", "error")
+        return redirect(url_for('user_blueprint.register'))
+        # return render_template("register.html", data = {"msg": "El usuario y/o email ya existen"}), 400
     
     new_user = User(id, username, email, password)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify({"msg": "Usuario registrado exitosamente"}), 201
+    # render_template("login.html", data = {"msg": "Usuario registrado exitosamente"}), 201
+    flash('Usuario registrado exitosamente', 'success')
+    return redirect(url_for('user_blueprint.login'))
+
+@user_route.route('/login')
+def loginForm():
+    return render_template("login.html"), 200
 
 @user_route.route('/login', methods=['POST'])
 def login():
     try:
-        data = user_login_schema.load(request.json)
+        data = user_login_schema.load(request.form)
     except ValidationError as err:
-        return jsonify({"msg": "Error de validación", "errors": err.messages}), 400
+        # return render_template("login.html", data = {"msg": "Error de validación"}), 400
+        # return jsonify({"msg": "Error de validación", "errors": err.messages}), 400
+        flash('Error de validación', 'error')
+        return redirect(url_for('user_blueprint.login'))
     
     username_or_email = data['username']
     password = data['password']
@@ -40,10 +64,20 @@ def login():
         user = User.query.filter_by(username=username_or_email).first()
 
     if user and user.check_password(password):
-        access_token = create_access_token(identity=user.id)
-        return jsonify(access_token=access_token), 200
+        # access_token = create_access_token(identity=user.id)
+        # return jsonify(access_token=access_token), 200
+        session['user_id'] = user.id
+        # return render_template("index.html"), 200
+        return redirect(url_for('index'))
+    
+    # return jsonify({"msg": "Usuario o contraseña incorrectos"}), 401
+    flash('Usuario o contraseña incorrectos', 'error')
+    return redirect(url_for('user_blueprint.login'))
 
-    return jsonify({"msg": "Usuario o contraseña incorrectos"}), 401
+@user_route.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('user_blueprint.login'), code=302)
 
 @user_route.route('/profile', methods=['GET'])
 @jwt_required()

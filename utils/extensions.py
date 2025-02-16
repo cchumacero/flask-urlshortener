5d28b70817bd0f6c30fe7994299eecc7c521ae65
cchumacero <cnchumacero@gmail.com>
2025-02-16 14:29:14 -0300

    Add dependencies for manage authentication

--- utils/extensions.py
+++ utils/extensions.py
@@ -4,6 +4,8 @@ from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_jwt_extended import JWTManager
 from flask_limiter.util import get_remote_address
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 import os
 
 db = SQLAlchemy()
@@ -15,3 +17,5 @@ limiter =  Limiter(
     default_limits=["1000 per day", "100 per hour"] 
 )
 jwt = JWTManager()
+login_manager = LoginManager()
+csrf = CSRFProtect()
